353c5af9889cc519e77063ea1c7c43b9504c9f11
Hong BUI <41295407+HONGMINHBUI@users.noreply.github.com>
2026-02-20 09:09:23 +0000

    Add files via upload

---
+++ MOUSEKEYBDHARDWAREINPUT.cs
@@ -0,0 +1,22 @@
+using System.Runtime.InteropServices;
+
+namespace OCC.UI.TestingFramework.Utility.InputSimulation
+{
+#pragma warning disable 649
+    /// <summary>
+    /// The combined/overlayed structure that includes Mouse, Keyboard and Hardware Input message data (see: http://msdn.microsoft.com/en-us/library/ms646270(VS.85).aspx)
+    /// </summary>
+    [StructLayout(LayoutKind.Explicit)]
+    struct MOUSEKEYBDHARDWAREINPUT
+    {
+        [FieldOffset(0)]
+        public MOUSEINPUT Mouse;
+
+        [FieldOffset(0)]
+        public KEYBDINPUT Keyboard;
+
+        [FieldOffset(0)]
+        public HARDWAREINPUT Hardware;
+    }
+#pragma warning restore 649
+}
